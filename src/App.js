150c6a358a1f573e9a3c4d1ce04dc1a93372eee6
SAM ADKINS <sjamesadkins@gmail.com>
2024-03-25 14:46:27 -0500

    logo, bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import laurels from "./Images/laurels.jpeg"
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+        <img src={laurels} alt="laurels img" height="700 px"/>
     </div>
   );
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+
+
+const Navbar = () => {
+
+}
+
+export default Navbar;
+
+
